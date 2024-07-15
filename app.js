d88b880852935a301f1a71d36695fcf33e1e3023
kabilan <kabilankabi386@gmail.com>
2024-07-16 00:06:32 +0530

    update backend

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
+require('dotenv').config();
 const multer = require('multer');
 const path = require('path');
-
+const cors = require('cors');
 const app = express();
-
+const db = require('./db.js');
+const Image = require('./imageModal.js'); 
+app.use(cors())
 // Set storage engine
 const storage = multer.diskStorage({
     destination: './uploads/',
-    filename: function(req, file, cb){
+    filename: function (req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
     }
 });
@@ -16,55 +19,64 @@ const storage = multer.diskStorage({
 const upload = multer({
     storage: storage,
     limits: { fileSize: 5000000 }, // 1MB limit
-    fileFilter: function(req, file, cb){
+    fileFilter: function (req, file, cb) {
         checkFileType(file, cb);
     }
-}).single('myFile'); 
+}).single('file');
 
 // Check file type
-function checkFileType(file, cb){
+function checkFileType(file, cb) {
     // Allowed ext
-    const filetypes = /jpeg|jpg|png|gif/;
+    const filetypes = /jpg|png/;
     // Check ext
     const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
     // Check mime
     const mimetype = filetypes.test(file.mimetype);
 
-    if(mimetype && extname){
+    if (mimetype && extname) {
         return cb(null, true);
     } else {
         cb('Error: Images Only!');
     }
 }
 
+app.use('/images', express.static('uploads'));
+
 // Express route
 app.post('/upload', (req, res) => {
-    upload(req, res, (err) => {
-        if(err){
+    upload(req, res, async (err) => {
+        if (err) {
             if (err.code === 'LIMIT_FILE_SIZE') {
                 res.json({
-                    status : false,
-                    message : "File size is too large! Maximum size is 5MB"
+                    status: false,
+                    message: "File size is too large! Maximum size is 5MB"
                 });
             } else {
                 res.send(err);
             }
         } else {
-            if(req.file == undefined){
+            if (req.file == undefined) {
                 res.json({
-                    status : false,
-                    message : 'Error: No File Selected!'
+                    status: false,
+                    message: 'Error: No File Selected!'
                 });
             } else {
+
+                const newImage = new Image({
+                    url: req.file.filename
+                });
+
+                await newImage.save();
+
                 res.json({
-                    status : true,
-                    message : `File Uploaded: ${req.file.filename}`
+                    status: true,
+                    message: `File Uploaded: ${req.file.filename}`
                 });
             }
         }
     });
 });
 
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 3030;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
---
+++ imageModal.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const imageSchema = new mongoose.Schema({
+    url: String, // Assuming you're storing URLs of images
+    dbState: {
+        type : Boolean,
+        default: false,
+    }
+});
+
+const Image = mongoose.model('Image', imageSchema);
+
+module.exports = Image;
